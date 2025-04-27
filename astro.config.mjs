6ef0c5d2139e7a7a2be5014519933a0e48f88ac0
Paul Christoforo <cstrophic@hotmail.com>
2025-04-26 18:33:12 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    site: 'https://robert-sanford-hvac.netlify.app',
-    base: '/',
-    integrations: [tailwind()]
+  output: 'static',
+  build: {
+    inlineStylesheets: 'never'
+  }
 });
