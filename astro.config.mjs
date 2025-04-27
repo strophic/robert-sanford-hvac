5f158889975d37b1bd3534feff0ffd4d20d2bbb7
Paul Christoforo <cstrophic@hotmail.com>
2025-04-26 19:49:14 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: 'static',
   integrations: [tailwind()],
+  site: 'https://robertsanfordhvac.com',
+  base: '/',
 });
